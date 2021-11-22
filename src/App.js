94e64587381517ceebf15ed6b7742dee423f7917
Adriel <adriel.fariasan@gmail.com>
2021-11-22 16:19:03 -0300

    Atualiza App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Editor from "./components/Editor";
+import Previewer from "./components/Previewer";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
-  return <main></main>;
+  const [inputText, setInputText] = useState(`# Start typing...`);
+  return (
+    <>
+      <main>
+        <Container>
+          <Editor inputText={inputText} setInputText={setInputText} />
+          <Previewer inputText={inputText} />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
